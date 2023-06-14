1cb094b485d4d71cb237041b567984943cf0ad4b
Hossam Mahmoud <nhem318@gmail.com>
2023-06-14 14:03:37 +0200

    modify infinite_add.py file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+
 import sys
 
 def add_arg(argv):
-    sum = 0
+    total_sum = 0
     length = len(argv)
     if length > 1:
-        for i in range(1,length):
-            sum += int(argv[i])
-    print("{:d}".format(sum))
+        for i in range(1, length):
+            total_sum += int(argv[i])
+    print("{:d}".format(total_sum))
 
 if __name__ == "__main__":
     add_arg(sys.argv)
