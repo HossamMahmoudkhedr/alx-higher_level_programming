61dfcb2bcfa7f5ea6c8287fd508f40cb4af3735c
Hossam Mahmoud <nhem318@gmail.com>
2023-06-14 12:07:39 +0200

    modify calculation.py file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add , sub, mul, div
     a = 10
     b = 5
-    from calculator_1 import add , sub, mul, div
-    add_op = add(a, b)
-    sub_op = sub(a, b)
-    mul_op = mul(a, b)
-    div_op = div(a, b)
-    print("{:d} + {:d} = {:d}".format(a ,b ,add_op))
-    print("{:d} - {:d} = {:d}".format(a ,b ,sub_op))
-    print("{:d} * {:d} = {:d}".format(a ,b ,mul_op))
-    print("{:d} / {:d} = {:d}".format(a ,b ,div_op))
+    print("{:d} + {:d} = {:d}".format(a ,b ,add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a ,b ,sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a ,b ,mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a ,b ,div(a, b)))
