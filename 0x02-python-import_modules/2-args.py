c7b54e5b562598495781934e0975d4a358bc4bd3
Hossam Mahmoud <nhem318@gmail.com>
2023-06-14 13:04:40 +0200

    modify 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_arg(argv):
-    length = len(argv)
+    length = len(argv) - 1
     if length == 1:
         print("{:d} argument:".format(length))
     elif length == 0:
