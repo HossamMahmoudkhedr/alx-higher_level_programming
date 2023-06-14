18eb177a9cd654b68869eb52777cc36d22b593e5
Hossam Mahmoud <nhem318@gmail.com>
2023-06-14 13:07:22 +0200

    modify args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ def print_arg(argv):
         print("{:d} arguments.".format(length))
     else:
         print("{:d} arguments:".format(length))
-    
-    if length >= 0:
+    length += 1
+    if length > 0:
         for i in range(1,length):
             print("{:d}: {:s}".format(i, argv[i]))
 if __name__ == "__main__":
