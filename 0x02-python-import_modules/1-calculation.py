4bf5a5a57805e7d21e575dd5179f1d8deae1fce8
Hossam Mahmoud <nhem318@gmail.com>
2023-06-14 12:06:10 +0200

    modify calculation.py file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add , sub, mul, div
     a = 10
     b = 5
+    from calculator_1 import add , sub, mul, div
     add_op = add(a, b)
     sub_op = sub(a, b)
     mul_op = mul(a, b)
