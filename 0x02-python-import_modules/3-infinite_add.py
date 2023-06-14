b5c4e38b00dfb2f0c905bc7dc098d71492976563
Hossam Mahmoud <nhem318@gmail.com>
2023-06-14 13:54:13 +0200

    add infinite_add.py file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_arg(argv):
+    sum = 0
+    length = len(argv)
+    if length > 1:
+        for i in range(1,argv):
+            sum += argv[i]
+    print("{:d}".format(sum))
