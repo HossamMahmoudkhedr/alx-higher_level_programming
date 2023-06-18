1545e042e77f7aeeac6d6da901989c0c5a57b74e
HossamMahmoudKhedr <nhem318@gmail.com>
2023-06-18 15:37:03 +0200

    add a new directory

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    newMatrix = []
+    l = 0
+    for row in matrix:
+        newMatrix.append([])
+        for i in row:
+            newMatrix[l].append(i*i)
+        l += 1
+    
+    return newMatrix
