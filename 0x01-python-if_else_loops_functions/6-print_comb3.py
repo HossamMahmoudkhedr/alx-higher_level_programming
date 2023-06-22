93d1efe5bc45fa68375f2ea0acfa15ecd8f34cfc
HossamMahmoudKhedr <nhem318@gmail.com>
2023-06-22 07:17:25 +0200

    add print_comb3.py file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(1, 10):
-        if i == j:
+        if i == j or i > j:
             continue
-        print("{}{}, ".format(i, j), end="")
+        elif i == 8:
+            print("{}{}".format(i, j), end="")
+        else:
+            print("{}{}, ".format(i, j), end="")
