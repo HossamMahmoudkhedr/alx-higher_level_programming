7e001666d71cf561a3ddf94da17904d2b83ade80
HossamMahmoudKhedr <nhem318@gmail.com>
2023-06-25 12:34:50 +0200

    add is_palindrome.c file

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int is_palindrome(listint_t **head)
+{
+    listint_t *temp, *first;
+    int *numbers = malloc(2 * sizeof(int));
+    int length = 0;
+    int i = 0;
+    int l = 0;
+    int result = 1;
+    temp = *head;
+
+    if (temp == NULL)
+    {
+        return (1);
+    }
+
+    while (temp != NULL)
+    {
+        length++;
+        temp = temp->next;
+    }
+    numbers = realloc(numbers, length * sizeof(int));
+
+    temp = *head;
+    while (temp != NULL)
+    {
+        numbers[i] = temp->n;
+        i++;
+        temp = temp->next;
+    }
+    
+    i = i - 1;
+    while (i != (length / 2))
+    {
+        if (numbers[i] != numbers[l])
+        {
+            result = 0;
+            break;
+        }
+        i--;
+        l++;
+    }
+    return (result);
+}
