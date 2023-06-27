1c4b39077bb2b5b65a28892e87b4774d2a4eb2d4
HossamMahmoudKhedr <nhem318@gmail.com>
2023-06-27 17:52:28 +0200

    modify 2-square.py file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,9 +12,8 @@ class Square:
         Args:
             size (int): the size of the square
         """
-        try:
-            self.__size = size
-        except TypeError:
-            raise print("size must be an integer")
-        except ValueError:
-            raise print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
