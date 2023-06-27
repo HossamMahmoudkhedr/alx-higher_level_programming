cff12acc0b92c38c9ca9f16e4e46eab6bedd0fb1
HossamMahmoudKhedr <nhem318@gmail.com>
2023-06-27 18:38:58 +0200

    modify 5-square.py file

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -34,9 +34,9 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        if self.__size == 0:
-            print()
         for i in range(self.__size):
             for l in range(self.__size):
                 print('#', end="")
+            print()
+        if self.__size == 0:
             print()
