2d039da05ff83790acb470dc7c2d84fad680cd2a
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-01 20:26:44 +0200

    add add_integer.py file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""This function adds two integers"""
+
+
 def add_integer(a, b=98):
+    """Return the value of adding a and b
+    
+    Args: 
+        a: the first integer
+        b: the second integer
+
+    Returns:
+        Sum of the two arguments
+
+    Raises:
+        TypeError: if the argument is not an integer or float
+    """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
