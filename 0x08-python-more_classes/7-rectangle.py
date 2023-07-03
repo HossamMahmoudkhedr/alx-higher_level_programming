dcda101cb15b8a579606bbc8111eb4dccf87b5f8
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-03 20:05:29 +0200

    add 7-rectangle.py file

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""Rectangle class."""
+
+
+class Rectangle:
+    """Defines an rectangle class"""
+
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0, print_symbol="#"):
+        """Set a private instance attributes
+        Args:
+            width (int): the width of the rectangle
+            height (int): the height of the rectangle
+        """
+        Rectangle.number_of_instances += 1
+        self.width = width
+        self.height = height
+        self.print_symbol = print_symbol
+
+    @property
+    def width(self):
+        """Retrive the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrive the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__height == 0 or self.width == 0:
+            return 0
+        return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        if isinstance(self.print_symbol, list):
+            new_print_sympol = "["
+            for i in range(len(self.print_symbol)):
+                if i == len(self.print_symbol) - 1:
+                    new_print_sympol += "'{}']".format(self.print_symbol[i])    
+                else:
+                    new_print_sympol += "'{}', ".format(self.print_symbol[i])
+            self.print_symbol = new_print_sympol
+        rectangle = ""
+        if self.__width != 0 and self.__height != 0:
+            for i in range(self.__height):
+                rectangle += self.print_symbol * self.__width + "\n"
+        return rectangle
+
+    def __repr__(self):
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
