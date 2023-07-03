0d9ae6c17b5fcec017ecb9b4accb2dfa23bcb04a
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-03 18:55:22 +0200

    modify 3-rectangle.py file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
 
     def __str__(self):
         rectangle = ""
-
-        for i in range(self.__height):
-                rectangle += '#' * self.__width + "\n"
+        if self.__width != 0 and self.__height != 0:
+            for i in range(self.__height):
+                    rectangle += '#' * self.__width + "\n"
         return rectangle
