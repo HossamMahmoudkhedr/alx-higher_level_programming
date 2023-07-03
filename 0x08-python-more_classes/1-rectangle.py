d6a3589eb123d3abca91a19c9dab6932cb8d149e
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-03 17:44:41 +0200

    Modify 1-rectangle.py file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -41,11 +41,3 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         
         self.__height = value
-
-
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
