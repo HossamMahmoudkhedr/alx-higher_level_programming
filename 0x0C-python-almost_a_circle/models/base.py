8f9a5bd65a3a86663fa5efa90a5a0321d0ff2166
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-14 09:58:20 +0200

    add rectangle class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,11 @@
 
 
 class Base:
+    '''
+        This class will manage the id attribute for all the classes.
+        Arguments:
+            @id: The id for a specific instance.
+    '''
     __nb_objects = 0
     def __init__(self, id=None):
         if id != None:
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" Rectangle class """
+from base import Base
+
+
+class Rectangle(Base):
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+    
+    def Validation(self, number, name):
+        if not isinstance(number, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if name == 'width' or name == 'height':
+            if number <= 0:
+                raise ValueError("{} must be > 0".format(name))
+        else:
+            if number < 0:
+                raise ValueError("{} must be >= 0".format(name))
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.Validation(width, "width")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.Validation(height, "height")
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.Validation(x, "x")
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.Validation(y, "y")
+        self.__y = y
