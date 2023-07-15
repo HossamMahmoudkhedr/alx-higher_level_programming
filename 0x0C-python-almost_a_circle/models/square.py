53c080ef78dca6851617a6998a249465f6314c5b
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-15 09:51:41 +0200

    the size setter in the square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,17 @@ from rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
-        self.size = size
+        self.__size = size
+
     def __str__(self):
-        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y,self.size )
+        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y,self.__size )
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        super().Validation(size, 'width')
+        self.__size = size
+
