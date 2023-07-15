87c2a1d4159ab082e64b28966b6229d78b7d34a4
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-15 07:54:02 +0200

    add update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,13 @@ class Rectangle(Base):
 
     def __str__(self):
         return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        try:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+            self.__y = args[4]
+        except IndexError:
+            pass
