db0ebd377ea3f94284f0e754306335b9fc8239b3
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-16 11:06:37 +0200

    add save_to_file function to the base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,5 +20,18 @@ class Base:
     def to_json_string(list_dictionaries):
         if list_dictionaries is None:
             return '[]'
-        
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        file_name = cls.__name__ + ".json"
+
+        content = []
+        if list_objs is not None:
+            for item in list_objs:
+                item = item.to_dictionary()
+                json_dict = json.loads(cls.to_json_string(item))
+                content.append(json_dict)
+
+        with open(file_name, mode="w") as fd:
+            json.dump(content, fd)
