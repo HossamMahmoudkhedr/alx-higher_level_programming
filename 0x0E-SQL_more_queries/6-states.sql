68cc4620c23724ebc277ec189120c3592ac239ca
HossamMahmoudKhedr <nhem318@gmail.com>
2023-08-16 13:06:31 +0200

    add states file

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates the table unique_id on your MySQL server.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS unique_id(
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256)
+);
