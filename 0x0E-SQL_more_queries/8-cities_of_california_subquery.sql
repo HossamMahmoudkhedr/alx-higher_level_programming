fc9d172f3a15038866c9b54096c2cf0f04590ddc
HossamMahmoudKhedr <nhem318@gmail.com>
2023-08-16 15:47:21 +0200

    add select id and name from cities table

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+--  lists all the cities of California that can be found in the database hbtn_0d_usa
+-- use hbtn_0d_usa database
+USE hbtn_0d_usa;
+-- Select the id and name from cities in ascending order
+SELECT id, name FROM cities ORDER BY id asc;
