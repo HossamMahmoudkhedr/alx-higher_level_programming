5593e21113a3c2e4a0f1730c0d4493042d61fd58
HossamMahmoudKhedr <nhem318@gmail.com>
2023-08-16 13:09:52 +0200

    modify states file

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
 -- Creates the table unique_id on your MySQL server.
-
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS unique_id(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL
-);
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
