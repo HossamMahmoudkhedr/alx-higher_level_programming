6816328b923b74c106e63477fac49cc89a10fb07
HossamMahmoudKhedr <nhem318@gmail.com>
2023-09-12 00:45:59 +0200

    add process

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-if (argv.length == 0) {
+if (process.argv.length == 0) {
 	console.log('No argument');
-} else if (argv.length == 1) {
+} else if (process.argv.length == 1) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
