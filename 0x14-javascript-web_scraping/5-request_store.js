8e43a6f947056ae9a120306f04a0372d4604abdd
HossamMahmoudKhedr <nhem318@gmail.com>
2023-10-25 16:43:16 +0200

    modify file:

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
 // A script that gets the contents of a webpage and stores it in a file.
 
-const URL = process.argv[2];
-const file = process.argv[3];
-let request = require('request');
-let fs = require('fs');
-
-request(URL, (error, body, response) => {
-	if (error) {
-		console.log(error);
-	} else {
-		const content = JSON.parse(body);
-		fs.writeFile(file, content, 'utf8');
-	}
-});
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
