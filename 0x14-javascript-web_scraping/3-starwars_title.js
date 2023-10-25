8338b416b9159620c3dfcb95a5de0f0770b4a3c2
HossamMahmoudKhedr <nhem318@gmail.com>
2023-10-25 14:45:17 +0200

    modify file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-// A script that prints the title of a Star Wars movie where the episode number matches a given integer.
+// A script that prints the title of a Star Wars movie where the episode num matches a given int
 
-const id = process.argv[2];
-const URL = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-let request = require('request');
-
-request(URL, (error, body) => {
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
 	if (error) {
-		console.log(error);
+		console.error(error);
 	} else {
 		console.log(JSON.parse(body).title);
 	}
