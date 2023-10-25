b23a9529b334f65f3a307b8bf4f89eba17c7f83c
HossamMahmoudKhedr <nhem318@gmail.com>
2023-10-25 16:05:25 +0200

    modify file

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
-// A script that prints the number of movies where the character “Wedge Antilles” is present.
+// A script that prints the number of movies where the character Wedge Antilles is present
 
-const request = require('request');
-const FILMS_URL = process.argv[2];
-const count = 0;
-request(FILMS_URL, (error, body, response) => {
+const args = process.argv;
+let reqURL = args[2];
+let request = require('request');
+request(reqURL, function (error, response, body) {
 	if (error) {
-		console.log(error);
+		console.log('error:', error); // Print the error if one occurred
 	} else {
-		const results = JSON.parse(body).results;
-		results.map((item) => {
-			for (let i = 0; i < item.characters.length; i++) {
-				if (item.characters[i].endsWith('18/')) {
-					count += 1;
+		let jso = JSON.parse(body);
+		let results = jso['results'];
+		let count = 0;
+		for (let i = 0; i < results.length; i++) {
+			let chars = results[i]['characters'];
+			for (let j = 0; j < chars.length; j++) {
+				let check18 = chars[j].endsWith('18/');
+				if (check18) {
+					count++;
 				}
 			}
-		});
+		}
 		console.log(count);
 	}
 });
