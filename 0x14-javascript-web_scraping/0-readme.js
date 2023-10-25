5a0fa604811a8faaa20ae9809c0a1c6c1bc2ee5a
HossamMahmoudKhedr <nhem318@gmail.com>
2023-10-25 11:49:25 +0200

    modify file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,3 +1,13 @@
-let arguments = process.argv;
+#!/usr/bin/node
+// A script that reads and prints the content of a file
 
-console.log(arguments[2]);
+const file = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(file, 'utf8', (error, data) => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(data);
+	}
+});
